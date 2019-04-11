e1367f46f9fdfe9c31e5c194046ca428f8979b40
DangerBlack <danger.recheng@hotmail.it>
2019-04-11 11:45:21 +0200

    Create SVGtoPNG.sh

---
+++ SVGtoPNG.sh
@@ -0,0 +1,18 @@
+
+svgexport ic_launcher_foreground.svg test/drawable-hdpi/ic_launcher_foreground.png 162:162
+svgexport ic_launcher_foreground.svg test/drawable-mdpi/ic_launcher_foreground.png 108:108
+svgexport ic_launcher_foreground.svg test/drawable-xhdpi/ic_launcher_foreground.png 216:216
+svgexport ic_launcher_foreground.svg test/drawable-xxhdpi/ic_launcher_foreground.png 324:324
+svgexport ic_launcher_foreground.svg test/drawable-xxxhdpi/ic_launcher_foreground.png 432:432
+
+svgexport ic_launcher_background.svg test/drawable-hdpi/ic_launcher_background.png 162:162
+svgexport ic_launcher_background.svg test/drawable-mdpi/ic_launcher_background.png 108:108
+svgexport ic_launcher_background.svg test/drawable-xhdpi/ic_launcher_background.png 216:216
+svgexport ic_launcher_background.svg test/drawable-xxhdpi/ic_launcher_background.png 324:324
+svgexport ic_launcher_background.svg test/drawable-xxxhdpi/ic_launcher_background.png 432:432
+
+svgexport logo.svg test/minmap-hdpi/launcher_icon.png 72:72
+svgexport logo.svg test/minmap-mdpi/launcher_icon.png 48:48
+svgexport logo.svg test/minmap-xhdpi/launcher_icon.png 96:96
+svgexport logo.svg test/minmap-xxhdpi/launcher_icon.png 144:144
+svgexport logo.svg test/minmap-xxxhdpi/launcher_icon.png 192:192
